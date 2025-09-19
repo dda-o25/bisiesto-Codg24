e4274000d6b3627d2d666c3a05538e226d589bd7
Codg24 <carlos.diaz@iteso.mx>
2025-09-19 01:09:09 -0600

    Primera solución

--- bisiesto.py
+++ bisiesto.py
@@ -1,15 +1,32 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Integrantes
+Carlos Osvaldo Díaz García
+
+Fecha
+18 de septiembre de 2025
+
+Determinar si un año es o no bisiesto. Según la 
+Wikipedia: “Año bisiesto es el divisible entre 4, 
+salvo que sea año secular -último de cada siglo, 
+terminado en «00»-, en cuyo caso ha de ser 
+divisible entre 400”.
 """
 
 # Declaraciones
-CONSTANTE = valor
+año_bisiesto = 4
+año_secular = 400
 
 # Entradas
-entrada = input()
+año = input("Introduzca un año: ")
+año_numero = int(año)
 
 # Proceso
-
-
-# Salidas
-print(salida)
+if año_numero % año_secular == 0:
+    # Salida
+    print(año_numero, "sí es un año bisiesto")
+elif año_numero % año_bisiesto == 0 and año[len(año)-2:len(año)] != "00":
+    # Salida
+    print(año_numero, "sí es un año bisiesto")
+else:
+    # Salida
+    print(año_numero, "no es un año bisiesto")
